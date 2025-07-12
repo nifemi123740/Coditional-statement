3536c379e1192389e6c098cf802f4962d3cc7173
nifemi123740 <deborahadegbite407@gmail.com>
2025-07-12 20:49:10 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+number=3
+if number > 0:
+    print(f"{number} is positive number")   
---
+++ main2.py
@@ -0,0 +1,6 @@
+buy_price=int(input("Enter the buy price: "))
+sell_price=int(input("Enter the sell price: "))
+if(sell_price > buy_price):
+    print("you made profit")
+else:
+    print("you made loss")
